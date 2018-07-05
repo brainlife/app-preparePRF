9776cd5911676fa64e45ea9cc244be02f4bc8060
stevengeeky <stevengeeky@gmail.com>
2018-07-05 12:04:25 -0400

    added mini visualizer to view pial surface/inflated surface

--- loader.py
+++ loader.py
@@ -23,3 +23,37 @@ def load_labels(filename):
             result[values[0]] = values[1:]
     
     return result
+
+def load_vtk(filename):
+    """
+    Load vtk image into a dictionary object
+    """
+    with open(filename) as f:
+        contents = f.read()
+    
+    result = dict()
+    lines = contents.splitlines()
+    mode = 'header'
+    point_idx = 0
+    num_points = None
+    
+    for line in lines:
+        if line.startswith('#'):
+            continue
+        values = re.split('[ ]+', line)
+        
+        if mode == 'points':
+            result[point_idx] = (values[0], values[1], values[2])
+            
+            point_idx += 1
+            if point_idx == num_points:
+                break
+        
+        if len(values) == 5:
+            result[values[0]] = values[1:]
+        
+        if values[0] == 'POINTS':
+            mode = 'points'
+            num_points = int(values[1])
+    
+    return result
--- main.py
+++ main.py
@@ -11,4 +11,13 @@ with open('config.json') as config_json:
 v1_path = os.path.join(config['freesurfer'], "label/lh.V1.label")
 v1 = loader.load_labels(v1_path)
 
-print(v1)
+lh_pial_path = os.path.join(config['freesurfer'], "surf/lh.pial.vtk")
+lh_pial = loader.load_vtk(lh_pial_path)
+
+rh_pial_path = os.path.join(config['freesurfer'], "surf/rh.pial.vtk")
+rh_pial = loader.load_vtk(rh_pial_path)
+
+num_points = len(lh_pial)
+print(num_points)
+for i in range(min(50, num_points)):
+    print(lh_pial[i], rh_pial[i])
